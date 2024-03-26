86b011aeafad0c4f577cf25bb6382f4e93071a9e
Phil <philippe.teigne@gmail.com>
2024-03-25 21:04:31 -0400

    create validation for table creation

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, presence: true, numericality: { greater_than: 0 }
+  validate :must_carry_one_clothing_type
+
+  def must_carry_one_clothing_type
+    if mens_apparel == false && womens_apparel == false
+      errors.add(:mens_apparel, "must carry either mens or womens apparel")
+    end
+  end
+
 end
