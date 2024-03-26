48120ab2794cd52565cd4c615e0e44f4b41393c4
Phil <philippe.teigne@gmail.com>
2024-03-25 21:04:14 -0400

    display validation errors when creating new store

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,14 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+new_store = Store.new
+
+puts "What is the store name"
+new_store.name = gets.chomp
+
+new_store.save
+if new_store.errors.size > 0
+  puts new_store.errors.full_messages
+else
+  Store.create(new_store)
+end
