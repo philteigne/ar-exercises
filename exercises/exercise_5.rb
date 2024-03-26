70a4ca14942ad8c455f695b4a757543c00c587ab
Phil <philippe.teigne@gmail.com>
2024-03-25 20:05:18 -0400

    sum total annual revenue, display stores with less than 1 million annual revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@sum = Store.sum(:annual_revenue)
+puts @sum
+
+@rich_stores = Store.where("annual_revenue < ?", 1000000)
+
+@rich_stores.each do |store|
+  puts store.name
+end
